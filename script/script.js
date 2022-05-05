8c9e72778c316832b6cb3361bbdbe09dd06a1b1e
vDawgg <83905032+vDawgg@users.noreply.github.com>
2022-05-05 13:52:58 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     console.log("Loaded document!")
     if(navigator.userAgent.match(/Iphone/i) || navigator.userAgent.match(/Android/i)) {
         console.log("Phone detected!")
-        $("#device").attr("content", "initial-scale = 0.8"); //.8 Makes the text readable but the table doesnt scale correctly on the index site
+        $("#device").attr("content", "initial-scale = 0.8");
     }
 })
 
