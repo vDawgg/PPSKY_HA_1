8e671a94837bb3cebdd4f48aad6f016742cc4870
vDawgg <83905032+vDawgg@users.noreply.github.com>
2022-05-05 13:28:42 +0200

    Added zoom-in when using mobile phones. Made editing for progress better looking.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
-//Example text
+$(document).ready(function () {
+    console.log("Loaded document!")
+    if(navigator.userAgent.match(/Iphone/i) || navigator.userAgent.match(/Android/i)) {
+        console.log("Phone detected!")
+        $("#device").attr("content", "initial-scale = 0.8"); //.8 Makes the text readable but the table doesnt scale correctly on the index site
+    }
+})
+
+function showProgress() {
+    let val = $("#range").val()
+    $("#progress").css('width', val+"%")
+    $("#progress").text(val+"%")
+    $("#todo").css('width', (100-val)+"%")
+}
