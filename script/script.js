91186e9a5a871be83c17c2e57e81fb4f96b429df
vDawgg <veit.laule@gmail.com>
2022-05-06 19:19:07 +0200

    Switched zoom-in to functionality included in bootstrap, changed wording in nav and deleted TODO id to make website scale correctly on mobile. Added length restriction to text fields.

--- script/script.js
+++ script/script.js
@@ -1,11 +1,3 @@
-$(document).ready(function () {
-    console.log("Loaded document!")
-    if(navigator.userAgent.match(/Iphone/i) || navigator.userAgent.match(/Android/i)) {
-        console.log("Phone detected!")
-        $("#device").attr("content", "initial-scale = 0.8");
-    }
-})
-
 function showProgress() {
     let val = $("#range").val()
     $("#progress").css('width', val+"%")
